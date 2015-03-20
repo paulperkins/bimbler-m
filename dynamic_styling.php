661ab7a9e9a601a7fd9e025c5db54c032dee2b08
paulperkins <paul@paulperkins.net>
2015-03-20 17:26:02 +1000

    Added comment button.

--- dynamic_styling.php
+++ dynamic_styling.php
@@ -9,6 +9,8 @@
 	echo '.badge { background-color: ' . ot_get_option('color-1') . ' ! important; }' . PHP_EOL;
 	echo '.nav-justified li.active a { border-bottom-color: ' . ot_get_option('color-1') . '!important; }' . PHP_EOL;
 
+	//echo '.comment-post-button {background-image: linear-gradient(to bottom,#f0ad4e 0,#eb9316 100%); }' . PHP_EOL;
+	echo '.comment-post-button {background-color: ' . ot_get_option('color-1') . '; }' . PHP_EOL;
 	
 	
 	echo '</style>' . PHP_EOL;
