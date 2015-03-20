dcf80e065f08a137c17084731f5175f5868591ba
paulperkins <paul@paulperkins.net>
2015-03-20 17:42:03 +1000

    And again...

--- events.php
+++ events.php
@@ -1932,7 +1932,10 @@
 
 				// For the first upcoming event display a full-width map.
 //				if ($first && ('upcoming' == $when)) {
-				if ($first && (!empty ($addr = get_venue_address($post->ID)))) {
+
+				$addr = get_venue_address($post->ID);
+				
+				if ($first && (!empty ($addr))) {
 					$content .= '				<h4 style="text-overflow: none!important;">' . $post->post_title . '</h4>' . PHP_EOL;
 					$content .= '				<p><strong>' . date ($time_str, strtotime($event_date)) . '</strong></p>' . PHP_EOL;
 					$content .= '				<p style="white-space: normal!important; text-overflow: initial!important; overflow: auto!important;">' . $excerpt . '</p>' . PHP_EOL;
