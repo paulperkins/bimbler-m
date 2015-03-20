7ea34e6449542bc64aa780f7ca6be554ffaa50c3
paulperkins <paul@paulperkins.net>
2015-03-20 17:30:22 +1000

    Added navbar text colour.

--- dynamic_styling.php
+++ dynamic_styling.php
@@ -5,6 +5,8 @@
 	echo '<style type="text/css">' . PHP_EOL;
 
 	echo '.navbar-header { background-color: ' . ot_get_option('color-1') . '; }' . PHP_EOL;
+	echo '.navbar-header span { color: #000!important; }' . PHP_EOL;
+	echo '.navbar-brand { color: #000!important; }' . PHP_EOL;
 	echo '[data-notifications]:after { background: ' . ot_get_option('color-1') . '; }' . PHP_EOL;
 	echo '.badge { background-color: ' . ot_get_option('color-1') . ' ! important; }' . PHP_EOL;
 	echo '.nav-justified li.active a { border-bottom-color: ' . ot_get_option('color-1') . '!important; }' . PHP_EOL;
