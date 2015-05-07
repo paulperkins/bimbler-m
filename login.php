147c2744bb12b17d3ed8375d6e9a4698ef8ba88a
paulperkins <paul@paulperkins.net>
2015-05-07 19:55:11 +1000

    Removed commented-out HTML.

--- login.php
+++ login.php
@@ -25,8 +25,6 @@
 
 	<link rel="stylesheet" href="fa/css/font-awesome.min.css">
 	
-<!--  	<script src="bimbler.js"></script> -->
-		
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<meta name="description" content="<?php bloginfo ('description'); ?>" />
 	<meta name="author" content="" />
@@ -92,28 +90,6 @@ body {
 		<div class="container-fluid">      
 
 		<h4>Please log in:</h4>
-		<!--	<form method="post" action="<?php bloginfo('url') ?>/wp-login.php" class="wp-user-form" data-ajax="false" >
-				<div class="username">
-					<label for="user_login"><?php _e('Username'); ?>: </label>
-					<input type="text" name="log" value="<?php echo esc_attr(stripslashes($user_login)); ?>" size="20" id="user_login" tabindex="11" />
-				</div>
-				<div class="password">
-					<label for="user_pass"><?php _e('Password'); ?>: </label>
-					<input type="password" name="pwd" value="" size="20" id="user_pass" tabindex="12" />
-				</div>
-				<div class="login_fields">
-					<div class="rememberme">
-						<label for="rememberme">
-							<input type="checkbox" name="rememberme" value="forever" checked="checked" id="rememberme" tabindex="13" /> Remember me
-						</label>
-					</div>
-					<?php //do_action('login_form'); ?>
-					<input type="submit" name="user-submit" value="<?php //_e('Login'); ?>" tabindex="14" class="user-submit" />
-					<input type="hidden" name="redirect_to" value="/m/<?php //echo $_SERVER['REQUEST_URI']; ?>" />
-					<input type="hidden" name="user-cookie" value="1" />
-				</div>
-			</form> -->
-		
 			<form method="post" role="form" action="<?php bloginfo('url') ?>/wp-login.php" class="wp-user-form">
 			
 				<div class="form-group">
@@ -142,13 +118,6 @@ body {
 						</label>
 					</div>				
 				</div>
-				
-				<!--  <div class="form-group">
-					<button type="submit" class="btn btn-primary btn-block btn-login">
-						<i class="entypo-login"></i>
-						Login
-					</button>
-				</div> -->
 
 				<?php do_action('login_form'); ?>
 				<input type="submit" name="user-submit" value="<?php _e('Login'); ?>" tabindex="14" class="user-submit" />
@@ -156,10 +125,7 @@ body {
 				<input type="hidden" name="user-cookie" value="1" />
 				
 			</form>		
-		
-		
-		
-		
+
 		</div>
 			
 		<!-- /Content -->
