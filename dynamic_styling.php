5978cc64fe23ecf073d22a537fcde8738519efa9
Paul Perkins <paul@paulperkins.net>
2015-06-18 18:38:06 +1000

    Fix-up call to ot_option_xxx.

--- dynamic_styling.php
+++ dynamic_styling.php
@@ -3,17 +3,20 @@
 <?php
 	echo '<!-- Dynamic Styling. -->' . PHP_EOL;
 	echo '<style type="text/css">' . PHP_EOL;
+	
+	//$color = ot_get_option('color-1');
+	$colour = '#dd9933';
 
-	echo '.navbar-header { background-color: ' . ot_get_option('color-1') . '; }' . PHP_EOL;
+	echo '.navbar-header { background-color: ' . $colour . '; }' . PHP_EOL;
 	echo '.navbar-header span { color: #000!important; }' . PHP_EOL;
 	echo '.navbar-brand { color: #000!important; }' . PHP_EOL;
 	echo '.navbar-toggle { background-color: #c0c0c0!important; }' . PHP_EOL;
-	echo '[data-notifications]:after { background: ' . ot_get_option('color-1') . '; }' . PHP_EOL;
-	echo '.badge { background-color: ' . ot_get_option('color-1') . ' ! important; }' . PHP_EOL;
-	echo '.nav-justified li.active a { border-bottom-color: ' . ot_get_option('color-1') . '!important; }' . PHP_EOL;
+	echo '[data-notifications]:after { background: ' . $colour . '; }' . PHP_EOL;
+	echo '.badge { background-color: ' . $colour . ' ! important; }' . PHP_EOL;
+	echo '.nav-justified li.active a { border-bottom-color: ' . $colour . '!important; }' . PHP_EOL;
 
 	//echo '.comment-post-button {background-image: linear-gradient(to bottom,#f0ad4e 0,#eb9316 100%); }' . PHP_EOL;
-	echo '.comment-post-button {background-color: ' . ot_get_option('color-1') . '; }' . PHP_EOL;
+	echo '.comment-post-button {background-color: ' . $colour . '; }' . PHP_EOL;
 	
 	
 	echo '</style>' . PHP_EOL;
