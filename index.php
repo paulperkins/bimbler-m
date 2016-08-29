525326b1762484ec76b714a2364409a4c363c974
Paul Perkins <paul@PPMB.lan>
2016-08-29 16:52:45 +1000

    Hardcode redirect URL in Javascript.

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 	require('../wp-blog-header.php');
 	
 	if (!is_user_logged_in()) {
-		echo '<script type="text/javascript">window.location.replace(\'' . bloginfo('url') . '/m/login.php\');</script>';
+		echo '<script type="text/javascript">window.location.replace(\'https://bimblers.com/m/login.php\');</script>'; // bloginfo ('url') not set here.
 	}
 
 ?>
